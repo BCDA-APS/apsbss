5f1c17cbaf45476adb34b7a4cc9cd47ca91dea1e
GitHub Action <action@github.com>
2022-01-05 23:36:18 +0000

    Add changes for c676f6a628722c0c5fc1823944391fba38884233

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("apsbss 1.5.4rc4 documentation");
+        $("li.nav-item-0 a").text("apsbss 1.5.4 documentation");
     }
 });
