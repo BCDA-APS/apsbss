94a4a08ed3d7f9f9ab1dc33bcbcea83707f13da6
Pete Jemian <prjemian@gmail.com>
2024-12-05 15:17:44 -0600

    MNT #14 update copyright

--- apsbss/apsbss_ioc.sh
+++ apsbss/apsbss_ioc.sh
@@ -196,7 +196,7 @@ esac
 # -----------------------------------------------------------------------------
 # :author:    Pete R. Jemian
 # :email:     jemian@anl.gov
-# :copyright: (c) 2017-2021, UChicago Argonne, LLC
+# :copyright: (c) 2017-2025, UChicago Argonne, LLC
 #
 # Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
 #
